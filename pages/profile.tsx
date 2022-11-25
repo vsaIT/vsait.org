7a8cef1dcde6116a52ba9b7df4538fba8442c32e
Shirajuki <shirajuki00@gmail.com>
2022-11-25 18:21:15 +0100

    feat: add initial styling for profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next';
 import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Footer from '@components/Footer';
+import Navigation from '@lib/components/Navigation';
+import { SmallHeader } from '@lib/components/Header';
 
 const Profile: NextPage = () => {
   const { status, data: session } = useSession({
@@ -13,16 +15,28 @@ const Profile: NextPage = () => {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <Head>
         <title>VSAiT | Profil</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to your profile {`${session?.user?.email}`}
-        </h1>
+      <Navigation />
+
+      <main className="flex w-full flex-1 flex-col items-center text-center">
+        <SmallHeader />
+
+        <div className="flex flex-col z-10 max-w-screen-xl mb-32 w-full gap-6 transform -translate-y-10">
+          <div className="flex w-full bg-white shadow-2xl rounded-2xl p-6">
+            <div></div>
+            <div>
+              <div></div>
+              <div></div>
+            </div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
       </main>
 
       <Footer />
