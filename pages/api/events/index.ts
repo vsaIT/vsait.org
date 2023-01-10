f5fcb852ae5b86b3e61022e0f0cd4f8ce5b0b948
Shirajuki <shirajuki00@gmail.com>
2023-01-10 15:00:51 +0100

    feat: update upcoming events quick view component

--- pages/api/events/index.ts
+++ pages/api/events/index.ts
@@ -1,18 +1,27 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@db';
 import { getErrorMessage } from '@lib/utils';
+import { isEmpty } from 'lodash';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { page } = req.query;
+  const page = isEmpty(req.query?.page) ? 0 : Number(req.query.page);
+  const upcoming = isEmpty(req.query?.upcoming) ? false : true;
   const take = 5;
   try {
     // Query all events, and then slice by current page :sweat_smile:
     const events = await prisma.event.findMany({
       // skip: page * take,
       // take: take,
-      where: {
-        isDraft: false,
-      },
+      where: upcoming
+        ? {
+            isDraft: false,
+            startTime: {
+              gte: new Date(),
+            },
+          }
+        : {
+            isDraft: false,
+          },
       orderBy: {
         startTime: 'desc',
       },
@@ -21,7 +30,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       },
     });
     const pages = Math.ceil(events.length / take);
-    const currentPage = Math.min(Number(page) || 1, pages);
+    const currentPage = Math.min(page || 1, pages);
 
     return res.status(200).json({
       events: events.slice((currentPage - 1) * take, currentPage * take),
