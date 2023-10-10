60170638edbb9a1f4ed647a0900847f2db1377d4
Tommy Luu <tommydl@stud.ntnu.no>
2023-10-10 22:50:36 +0200

    update plugin

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: true,
-};
+  
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.gitignore$/,
+      loader: 'ignore-loader',
+    })
+    return config
+  },
+}
