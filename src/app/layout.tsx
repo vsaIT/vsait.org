6b8f26a527b49477fdc1f011af96125e1b70b6db
Tommy Luu <tommydl@stud.ntnu.no>
2024-02-11 15:57:51 +0100

    dynamic head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,48 +1,30 @@
-'use client';
-import '@/styles/globals.css';
-import 'react-toastify/dist/ReactToastify.css';
-import { ReactNode, Suspense } from 'react';
-import LoadingIndicator from '@/components/LoadingIndicator';
-import Head from 'next/head';
-import { mapPageTitle } from '@/lib/utils';
-import { usePathname } from 'next/navigation';
-import { SessionProvider } from 'next-auth/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ToastContainer } from 'react-toastify';
 import Footer from '@/components/Footer';
+import Head from '@/components/Head';
+import LoadingIndicator from '@/components/LoadingIndicator';
 import { Navigation } from '@/components/Navigation';
-
-const queryClient = new QueryClient();
+import '@/styles/globals.css';
+import { ReactNode, Suspense } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Providers from './Providers';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  const currPath = usePathname();
   return (
     <html lang='no'>
-      <Head>
-        <title>{currPath ? mapPageTitle(currPath) : 'Invalid url'}</title>
-        <meta
-          name='description'
-          content='Vietnamese Student Association in Trondheim'
-        />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+      <Head />
       <body>
-        <SessionProvider refetchInterval={5 * 60}>
-          <QueryClientProvider client={queryClient}>
-            <Navigation />
-            <div className='flex min-h-screen flex-col items-center justify-center'>
-              <Suspense fallback={<LoadingIndicator />}>
-                <main className='flex w-full flex-1 flex-col items-center text-center'>
-                  {/* <WithAuth options={{redirectTo: "/login"}}> */}
-                  {children}
-                  {/* </WithAuth> */}
-                </main>
-              </Suspense>
-            </div>
-            <Footer />
-          </QueryClientProvider>
+        <Providers>
+          <Navigation />
+          <div className='flex min-h-screen flex-col items-center justify-center'>
+            <Suspense fallback={<LoadingIndicator />}>
+              <main className='flex w-full flex-1 flex-col items-center text-center'>
+                {children}
+              </main>
+            </Suspense>
+          </div>
+          <Footer />
           <ToastContainer />
-        </SessionProvider>
+        </Providers>
       </body>
     </html>
   );
