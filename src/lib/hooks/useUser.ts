e12435d909872e779333b13e2e18d7611ea0b4fd
Tommy Luu <tommydl@stud.ntnu.no>
2024-04-21 23:58:37 +0200

    increase refresh interval

--- src/lib/hooks/useUser.ts
+++ src/lib/hooks/useUser.ts
@@ -5,7 +5,8 @@ import { fetcher } from '../utils';
 export function useUser(id: string | undefined) {
   const { data, error, isLoading } = useSWR<UserType>(
     `/api/user/${id}`,
-    fetcher
+    fetcher,
+    { refreshInterval: 1000 * 60 * 10 } // 10 minutes
   );
 
   return {
