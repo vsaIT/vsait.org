6fc4c72778d83a17ad216ecb9cb672c8c37520f7
Tommy Luu <tommydl@stud.ntnu.no>
2025-10-04 22:02:43 +0200

    feat: add ImagePreview and SlideCheckbox components; enhance AdminEventsView with image upload and slider functionality

--- src/app/admin/events/[eventid]/page.tsx
+++ src/app/admin/events/[eventid]/page.tsx
@@ -2,6 +2,7 @@
 import { useEvent } from '@/lib/hooks/useEvent';
 import { Button } from '@/components/Input';
 import { FormInput, FormImageInput, SelectField } from '@/components/Form';
+import ImagePreview from '../../../../components/ImagePreview';
 //import { TextEditor } from '@/components/Input';
 import { useCallback, useState } from 'react';
 import { timeEnd } from 'node:console';
@@ -10,6 +11,7 @@ import { useForm } from 'react-hook-form';
 import { EventType } from '@/types';
 import { EventType as EventTypeOptions } from '@prisma/client';
 import { isoToOsloTimestring, osloTimeStringToUtcIso } from '@/lib/utils';
+import SlideCheckbox from '@/components/Input/SlideCheckbox';
 
 type AdminEventsProps = {
   params: {
@@ -33,12 +35,21 @@ type TimeDataInput = {
 function AdminEventsView({ params }: AdminEventsProps): JSX.Element {
   const { eventid } = params;
   const { data, isLoading } = useEvent(eventid);
+  const [image, setImage] = useState<File | null>(null);
   const {
     register,
     handleSubmit,
     setValue,
+    watch,
     formState: { errors },
   } = useForm<EventType>({ defaultValues: data?.event });
+
+  const watchEventdata = watch();
+
+  const sliderCheckboxes = [
+    { id: 'isDraft', label: 'Kladd (ikke synlig for brukere)' },
+    { id: 'isCancelled', label: 'Avlyst' },
+  ];
 
   const timeDataInputs: Array<TimeDataInput> = [
     {
@@ -140,7 +151,7 @@ function AdminEventsView({ params }: AdminEventsProps): JSX.Element {
         onSubmit={handleSubmit(onSubmit)}
         className='rounded-xl bg-white p-6'
       >
-        <div className='flex w-full flex-row items-stretch justify-evenly space-x-3'>
+        <div className='flex h-full w-full flex-row items-stretch justify-evenly space-x-3'>
           <div className='col-span-full w-2/3 flex-col space-y-4 rounded-xl border border-stone-300 p-6'>
             <div className=''>
               <h2 className='text-xl'>Detaljer:</h2>
@@ -155,20 +166,27 @@ function AdminEventsView({ params }: AdminEventsProps): JSX.Element {
 
             <div>
               <h2>Banner:</h2>
-              <div className='max-h-70 flex justify-evenly gap-3 border border-stone-300 p-2'>
-                <div className={'w-1/2 flex-col'}>
-                  <h3>Nåværende:</h3>
-                  {/*<img src={event?.event.image || ''} alt="Forhåndsvisning" className="mt-2 max-h-45 rounded-lg object-contain" />*/}
-                  {data?.event.image}
+              <div className='max-h-70 flex-col gap-3'>
+                <div className={'w-[38%] flex-col justify-start'}>
+                  <h3 className='text-sm font-light italic'>
+                    Forhåndsvisning:
+                  </h3>
+                  <ImagePreview
+                    src={
+                      image
+                        ? URL.createObjectURL(image)
+                        : data?.event.image || undefined
+                    }
+                    alt='Forhåndsvisning'
+                  />
                 </div>
-                <div className={'w-1/2 flex-col'}>
-                  <h3>Endre:</h3>
+                <div className='mt-1 w-1/2'>
                   <FormImageInput
                     key={'image'}
                     label={'Bilde'}
                     type={'file'}
                     accept={'image/*'}
-                    {...register('image')}
+                    onChange={(e) => setImage(e.target.files?.[0] || null)}
                   />
                 </div>
               </div>
@@ -188,51 +206,63 @@ function AdminEventsView({ params }: AdminEventsProps): JSX.Element {
               {...register('location', { required: true })}
             />
 
-            <div>
-              <SelectField
-                label='Arrangementstype'
-                name='arrType'
-                defaultValue={data?.event.eventType || ''}
-                options={eventTypeOptions}
-                onChange={(e) =>
-                  setValue('eventType', e.target.value as EventTypeOptions)
-                }
-              />
+            <SelectField
+              label='Arrangementstype'
+              name='arrType'
+              defaultValue={data?.event.eventType || ''}
+              options={eventTypeOptions}
+              onChange={(e) =>
+                setValue('eventType', e.target.value as EventTypeOptions)
+              }
+            />
+            {/* Sliders */}
+            <div className='grid grid-cols-2'>
+              {sliderCheckboxes.map((checkbox) => (
+                <SlideCheckbox
+                  key={checkbox.id}
+                  id={checkbox.id}
+                  label={checkbox.label}
+                  {...register(checkbox.id as keyof EventType)}
+                />
+              ))}
             </div>
           </div>
-          <div className='col-span-full w-1/3 flex-col space-y-4 rounded-xl border border-stone-300 p-6'>
+          <div className='flex h-full w-1/3 flex-col justify-between rounded-xl border border-stone-300 p-6'>
             <div>
-              <h2 className='text-xl'>Dato og tid:</h2>
-            </div>
-            {timeDataInputs.map((inputFieldData, index) => (
-              <div key={index} className='flex flex-col space-y-1 pb-2'>
-                <h2 className='text-l p-2'>{inputFieldData.name}</h2>
-                <div className='flex flex-auto flex-row space-x-3'>
-                  <FormInput
-                    key={inputFieldData.attr}
-                    {...inputFieldData.date}
-                    {...register(inputFieldData.attr as keyof EventType, {
-                      required: true,
-                    })}
-                  />
-                </div>
+              <div>
+                <h2 className='text-xl'>Dato og tid:</h2>
               </div>
-            ))}
-            <div className='justify-left mt-6 flex w-full'>
+              {timeDataInputs.map((inputFieldData, index) => (
+                <div key={index} className='flex flex-col space-y-1 pb-2'>
+                  <h2 className='text-l p-2'>{inputFieldData.name}</h2>
+                  <div className='flex flex-auto flex-row space-x-3'>
+                    <FormInput
+                      key={inputFieldData.attr}
+                      {...inputFieldData.date}
+                      {...register(inputFieldData.attr as keyof EventType, {
+                        required: true,
+                      })}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className='flex w-full flex-col justify-evenly space-y-4 lg:flex-row lg:space-x-4 lg:space-y-0'>
               <Button
                 type='submit'
                 text='Lagre endringer'
-                className='px-6 py-2'
+                className='px-6 py-3'
               />
               <Button
                 type='button'
                 text='Slett arrangement'
-                className='ml-4 px-6 py-2'
+                className='px-6 py-3'
               />
             </div>
           </div>
         </div>
       </form>
+      <div className='p-4'>&nbsp;</div>
     </div>
   );
 }
--- src/components/Input/SlideCheckbox.tsx
+++ src/components/Input/SlideCheckbox.tsx
@@ -1,35 +1,38 @@
+import React from 'react';
+
 type SlideCheckboxProps = {
   className?: string;
   id: string;
   label?: string;
-  checked: boolean;
-  onChange?: () => void;
+  checked?: boolean;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
 };
 
-const SlideCheckbox = ({
-  id,
-  className = '',
-  label = '',
-  checked,
-  onChange,
-}: SlideCheckboxProps) => {
-  return (
-    <div className={'w-full py-5 text-left ' + className}>
-      <label
-        className='relative inline-flex cursor-pointer items-center'
-        htmlFor={id}
-      >
-        <input
-          id={id}
-          type='checkbox'
-          className='peer sr-only'
-          checked={checked}
-          onChange={onChange}
-        />
-        <div className="after:border-gray-300 peer h-6 w-11 rounded-full bg-placeholder after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:bg-white after:transition-all after:content-[''] peer-checked:bg-primary peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4"></div>
-        <span className='text-gray-900 ml-3 text-sm font-medium'>{label}</span>
-      </label>
-    </div>
-  );
-};
+const SlideCheckbox = React.forwardRef<HTMLInputElement, SlideCheckboxProps>(
+  ({ id, className = '', label = '', checked, onChange, ...rest }, ref) => {
+    return (
+      <div className={'w-full py-5 text-left ' + className}>
+        <label
+          className='relative inline-flex cursor-pointer items-center'
+          htmlFor={id}
+        >
+          <input
+            id={id}
+            type='checkbox'
+            className='peer sr-only'
+            checked={checked}
+            onChange={onChange}
+            ref={ref}
+            {...rest}
+          />
+          <div className="after:border-gray-300 peer h-6 w-11 rounded-full bg-placeholder after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:bg-white after:transition-all after:content-[''] peer-checked:bg-primary peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4"></div>
+          <span className='text-gray-900 ml-3 text-sm font-medium'>
+            {label}
+          </span>
+        </label>
+      </div>
+    );
+  }
+);
+SlideCheckbox.displayName = 'SlideCheckbox';
 export default SlideCheckbox;
